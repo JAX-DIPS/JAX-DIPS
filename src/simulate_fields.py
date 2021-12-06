91d6afcea525bb833d17bdd259d6e51f21ba867b
samirapakravan <pakravans90@gmail.com>
2021-12-06 00:11:26 -0800

    debugging

--- src/simulate_fields.py
+++ src/simulate_fields.py
@@ -83,6 +83,10 @@ def reinitialize_level_set(sstate: T,
     This function should be called every few iterations to maintain 
     the level set function.
     """
+    x = gstate.x; y = gstate.y; z = gstate.z
+    dx = x[2] - x[1]; dy = y[2] - y[1]; dz = z[2] - z[1]
+    dtau = jnp.min(jnp.array([dx, dy, dz])) / f64(30.0)
+
     phi_0 = sstate.solution
     sgn_0 = jnp.sign(phi_0)
     phi_n = phi_0
@@ -91,7 +95,6 @@ def reinitialize_level_set(sstate: T,
     def step_phi_fn(i, sgn_phi_n):
         sgn_0, phi_n_ = sgn_phi_n
         hg_n = interpolate.godunov_hamiltonian(phi_n_, gstate)
-        dtau = f32(0.0001)
         phi_t_np1 = phi_n_ + dtau * jnp.multiply(sgn_0, hg_n)
         hg_np1 = interpolate.godunov_hamiltonian(phi_t_np1, gstate)
         phi_t_np2 = phi_t_np1 + dtau * jnp.multiply(sgn_0, hg_np1)
