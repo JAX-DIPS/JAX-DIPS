1e719f418f63bdd3b9549208a4f2ec1b16afd633
Rajesh K Ilango <rilango@gmail.com>
2022-09-23 09:37:00 -0700

    Add the ability to create checkpoint files and resume from checkpoint.
    Also the ability to stop training on Ctrl+c

--- src/poisson_solver_scalable.py
+++ src/poisson_solver_scalable.py
@@ -72,10 +72,10 @@ class SState:
 class SStateFn:
     u_0_fn           :  Callable[..., Array]
     dir_bc_fn        :  Callable[..., Array]
-    phi_fn           :  Callable[..., Array] 
-    mu_m_fn          :  Callable[..., Array] 
-    mu_p_fn          :  Callable[..., Array] 
-    k_m_fn           :  Callable[..., Array] 
+    phi_fn           :  Callable[..., Array]
+    mu_m_fn          :  Callable[..., Array]
+    mu_p_fn          :  Callable[..., Array]
+    k_m_fn           :  Callable[..., Array]
     k_p_fn           :  Callable[..., Array]
     f_m_fn           :  Callable[..., Array]
     f_p_fn           :  Callable[..., Array]
@@ -86,12 +86,12 @@ class SStateFn:
 
 
 
-def setup(initial_value_fn :  Callable[..., Array], 
+def setup(initial_value_fn :  Callable[..., Array],
           dirichlet_bc_fn  :  Callable[..., Array],
-          lvl_set_fn       :  Callable[..., Array], 
-          mu_m_fn_         :  Callable[..., Array], 
-          mu_p_fn_         :  Callable[..., Array], 
-          k_m_fn_          :  Callable[..., Array], 
+          lvl_set_fn       :  Callable[..., Array],
+          mu_m_fn_         :  Callable[..., Array],
+          mu_p_fn_         :  Callable[..., Array],
+          k_m_fn_          :  Callable[..., Array],
           k_p_fn_          :  Callable[..., Array],
           f_m_fn_          :  Callable[..., Array],
           f_p_fn_          :  Callable[..., Array],
@@ -124,12 +124,15 @@ def setup(initial_value_fn :  Callable[..., Array],
         F_P   = None #sim_state_fn.f_p_fn(R)
         ALPHA = None #sim_state_fn.alpha_fn(R)
         BETA  = None #sim_state_fn.beta_fn(R)
-        sim_state = SState(PHI, U, DIRBC, MU_M, MU_P, K_M, K_P, F_M, F_P, ALPHA, BETA, None, None) 
+        sim_state = SState(PHI, U, DIRBC, MU_M, MU_P, K_M, K_P, F_M, F_P, ALPHA, BETA, None, None)
         return sim_state
 
-    def solve_fn(gstate, eval_gstate, sim_state, algorithm=0, switching_interval=3, Nx_tr=32, Ny_tr=32, Nz_tr=32, num_epochs=1000, multi_gpu=False, batch_size=131072):
-        U_sol, grad_u_mp, grad_u_mp_normal_to_interface, epoch_store, loss_epochs = poisson_kernels_cellular_core_algorithms_scalable.poisson_solver(gstate, eval_gstate, sim_state, sim_state_fn, algorithm, switching_interval=switching_interval, Nx_tr=Nx_tr, Ny_tr=Ny_tr, Nz_tr=Nz_tr, num_epochs=num_epochs, multi_gpu=multi_gpu, batch_size=batch_size)
-
+    def solve_fn(gstate, eval_gstate, sim_state, algorithm=0, switching_interval=3, Nx_tr=32, Ny_tr=32, Nz_tr=32, num_epochs=1000, multi_gpu=False, batch_size=131072, checkpoint_dir="./checkpoints"):
+        U_sol, grad_u_mp, grad_u_mp_normal_to_interface, epoch_store, loss_epochs = \
+            poisson_kernels_cellular_core_algorithms_scalable.poisson_solver(gstate, eval_gstate, sim_state, sim_state_fn, algorithm,
+                                                                             switching_interval=switching_interval,
+                                                                             Nx_tr=Nx_tr, Ny_tr=Ny_tr, Nz_tr=Nz_tr,
+                                                                             num_epochs=num_epochs, multi_gpu=multi_gpu, batch_size=batch_size, checkpoint_dir=checkpoint_dir)
         return dataclasses.replace(sim_state, solution=U_sol, grad_solution=grad_u_mp, grad_normal_solution=grad_u_mp_normal_to_interface), epoch_store, loss_epochs
-    
+
     return init_fn, solve_fn
