fixing units and fixing learning rate

examples/biomolecules_GZ17/coefficients.py
@@ -16,9 +16,26 @@ def initial_value_fn(r):
     return 0.0
 
 
-@custom_jit
-def dirichlet_bc_fn(r):
-    return 0.0
+def get_dirichlet_bc_fn(atom_xyz_rad_chg):
+    psi_star_coeff = C / eps_p_r
+    kappa = (kappa_sq_p / eps_p_r)**0.5
+    def dirichlet_bc_fn(r):
+        x = r[0]
+        y = r[1]
+        z = r[2]
+        def psi_pairwise_kernel(carry, xyzsc):
+            psi, = carry
+            xc, yc, zc, sigma, chg = xyzsc
+            dst = jnp.sqrt( (x - xc)**2 + (y - yc)**2 + (z - zc)**2)  
+            dst = jnp.clip(dst, a_min=sigma*0.01)
+            tmp_psi = chg * jnp.exp(-dst * kappa) / dst
+            psi += jnp.nan_to_num(tmp_psi)
+            return (psi,), None
+        psi = 0.0
+        (psi,), _ = lax.scan(psi_pairwise_kernel, (psi,), atom_xyz_rad_chg)
+        return psi_star_coeff * psi
+    
+    return jit(dirichlet_bc_fn)
 
 
 
@@ -131,7 +148,25 @@ def get_psi_star(atom_xyz_rad_chg, EPS=1e-6):
         (psi,), _ = lax.scan(psi_pairwise_kernel, (psi,), atom_xyz_rad_chg)
         return psi_star_coeff * psi
     psi_vec_fn = vmap(psi_at_r)
-    return jit(psi_at_r), jit(psi_vec_fn)
+    
+    @custom_jit
+    def grad_psi_fn(r):
+        x = r[0]
+        y = r[1]
+        z = r[2]
+        def grad_psi_pairwise_kernel(carry, xyzsc):
+            grad_psi, = carry
+            xc, yc, zc, sigma, chg = xyzsc
+            dst = jnp.sqrt( (x - xc)**2 + (y - yc)**2 + (z - zc)**2)  
+            dst = jnp.clip(dst, a_min=0.01*sigma)
+            tmp_psi = chg * jnp.array([xc - x, yc - y, zc - z]) / dst**3
+            grad_psi += jnp.nan_to_num(tmp_psi)
+            return (grad_psi,), None
+        grad_psi = jnp.array([0., 0., 0.])
+        (grad_psi,), _ = lax.scan(grad_psi_pairwise_kernel, (grad_psi,), atom_xyz_rad_chg)
+        return psi_star_coeff * grad_psi
+    grad_vec_psi_fn = vmap(grad_psi_fn)
+    return jit(psi_at_r), jit(psi_vec_fn), jit(grad_psi_fn), jit(grad_vec_psi_fn)
 
 
 
@@ -217,7 +252,8 @@ def get_rho_fn(atom_xyz_rad_chg):
         def initialize(carry, xyzsc):
             rho, = carry
             xc, yc, zc, sigma, chg = xyzsc
-            rho += chg * jnp.exp( -((x-xc)**2 + (y-yc)**2 + (z-zc)**2) / (2*sigma*sigma) ) / ( (2*jnp.pi)**1.5 * sigma*sigma*sigma)
+            ch_sigma = 0.1 * sigma
+            rho += chg * jnp.exp( -((x-xc)**2 + (y-yc)**2 + (z-zc)**2) / (2*ch_sigma*ch_sigma) ) / ( (2*jnp.pi)**1.5 * ch_sigma*ch_sigma*ch_sigma)
             rho  = jnp.nan_to_num(rho)
             return (rho,), None
         fm = 0.0

examples/biomolecules_GZ17/free_energy.py
@@ -2,7 +2,7 @@ from jax import vmap, numpy as jnp
 from src import interpolate, geometric_integrations_per_point
 from examples.biomolecules_GZ17.units import *
 import pdb
-import numpy as onp
+from examples.biomolecules_GZ17.coefficients import get_rho_fn
 """
     * Based on equation 9 in `Eﬃcient calculation of fully resolved electrostatics around large biomolecules`
     
@@ -10,7 +10,7 @@ import numpy as onp
         'Numerical Considerations in the Computation of the Electrostatic Free Energy of Interaction within the Poisson-Boltzmann Theory'
 """
 
-def get_free_energy(gstate, psi_hat, atom_xyz_rad_chg):
+def get_free_energy(gstate, phi, psi_hat, atom_xyz_rad_chg, epsilon_grad_psi_sq, psi_solution, epsilon_grad_psi_star_sq, epsilon_grad_psi_hat_sq):
     
     #---- First term in the equation 
     xyz, rad_chg = jnp.split(atom_xyz_rad_chg, [3], axis=1)
@@ -18,7 +18,37 @@ def get_free_energy(gstate, psi_hat, atom_xyz_rad_chg):
     
     psi_hat_interp_fn = interpolate.nonoscillatory_quadratic_interpolation(psi_hat, gstate)                                
                                       
-    sfe = jnp.sum( psi_hat_interp_fn(xyz) * jnp.squeeze(chg)  ) * eC_per_Angs_to_kcal_per_mol_eC   # convert to kcal/mol/eC
+    sfe = jnp.sum( psi_hat_interp_fn(xyz) * jnp.squeeze(chg)  ) * e2_per_Angs_to_kcal_per_mol   # convert to kcal/mol/eC
+    
+    
+    
+    
+    # # method 2:
+    # epsilon_E_sq = 0.5 * epsilon_grad_psi_sq - 0.5 * epsilon_grad_psi_star_sq
+    # E_correction = epsilon_E_sq.sum() * (gstate.dx*gstate.dy*gstate.dz) * e2_per_Angs_to_kcal_per_mol * 1e-10
+    # sfe_2 = sfe - E_correction
+    
+    # method 3
+    # rho_fn = get_rho_fn(atom_xyz_rad_chg)
+    # chg_density = vmap(rho_fn)(gstate.R)
+    # integrand = chg_density * psi_solution - epsilon_E_sq
+    # integral_interp_fn = interpolate.nonoscillatory_quadratic_interpolation(integrand, gstate)
+    
+    # phi_interp_fn = interpolate.nonoscillatory_quadratic_interpolation(phi, gstate)
+    # get_vertices_of_cell_intersection_with_interface_at_point, is_cell_crossed_by_interface = geometric_integrations_per_point.get_vertices_of_cell_intersection_with_interface(phi_interp_fn)
+    # _, integrate_in_negative_domain_at_point = geometric_integrations_per_point.integrate_over_gamma_and_omega_m(get_vertices_of_cell_intersection_with_interface_at_point, is_cell_crossed_by_interface, integral_interp_fn)
+    
+    # phi_p_interp_fn = interpolate.nonoscillatory_quadratic_interpolation(-phi, gstate)
+    # get_vertices_of_cell_intersection_with_interface_at_point, is_cell_crossed_by_interface = geometric_integrations_per_point.get_vertices_of_cell_intersection_with_interface(phi_p_interp_fn)
+    # _, integrate_in_positive_domain_at_point = geometric_integrations_per_point.integrate_over_gamma_and_omega_m(get_vertices_of_cell_intersection_with_interface_at_point, is_cell_crossed_by_interface, integral_interp_fn)
+    
+    
+    # partial_integrals_negative = vmap(integrate_in_negative_domain_at_point, (0, None, None, None))(gstate.R, gstate.dx, gstate.dy, gstate.dz)
+    # partial_integrals_positive = vmap(integrate_in_positive_domain_at_point, (0, None, None, None))(gstate.R, gstate.dx, gstate.dy, gstate.dz)
+    # sfe_3 = ( jnp.sum(partial_integrals_negative) + jnp.sum(partial_integrals_positive) ) * eC_per_Angs_to_kcal_per_mol_eC
+    
+    
+    
     return sfe
     
     

examples/biomolecules_GZ17/main.py
@@ -61,7 +61,7 @@ def biomolecule_solvation_energy(file_name = 'pdb:1ajj.pqr', molecule_pqr_addres
     
     num_epochs = 1000
     
-    Nx_tr = Ny_tr = Nz_tr = 128                  # grid for training
+    Nx_tr = Ny_tr = Nz_tr = 64                  # grid for training
     Nx = Ny = Nz = 256                           # grid for level-set
     Nx_eval = Ny_eval = Nz_eval = 256            # grid for visualization
     
@@ -113,11 +113,12 @@ def biomolecule_solvation_energy(file_name = 'pdb:1ajj.pqr', molecule_pqr_addres
     eval_gstate = init_mesh_fn(exc, eyc, ezc)
 
     ###########################################################
+    dirichlet_bc_fn = get_dirichlet_bc_fn(atom_xyz_rad_chg)
     
     unperturbed_phi_fn = get_initial_level_set_fn(atom_xyz_rad_chg)  
     phi_fn = level_set.perturb_level_set_fn(unperturbed_phi_fn)
     
-    psi_star_fn, psi_star_vec_fn = get_psi_star(atom_xyz_rad_chg)
+    psi_star_fn, psi_star_vec_fn, grad_psi_star_fn, grad_psi_star_vec_fn = get_psi_star(atom_xyz_rad_chg)
     
     alpha_fn, beta_fn = get_jump_conditions(atom_xyz_rad_chg, psi_star_fn, phi_fn, eval_gstate.dx*0.01, eval_gstate.dy*0.01, eval_gstate.dz*0.01)
     
@@ -195,6 +196,7 @@ def biomolecule_solvation_energy(file_name = 'pdb:1ajj.pqr', molecule_pqr_addres
     chg_density = vmap(rho_fn)(eval_gstate.R)
     
     
+    
     psi_star = psi_star_vec_fn(eval_gstate.R)
     psi_hat = sim_state.solution
     def compose_psi_fn(r, psi_hat_r, psi_star_r):
@@ -205,8 +207,6 @@ def biomolecule_solvation_energy(file_name = 'pdb:1ajj.pqr', molecule_pqr_addres
     
     grad_u_jump = vmap(beta_fn)(eval_gstate.R)
     u_jump = vmap(alpha_fn)(eval_gstate.R)
-    
-    
     log = {'phi'  : eval_phi,
            'rho'  : chg_density, 
            'U'    : psi_solution,
@@ -218,7 +218,21 @@ def biomolecule_solvation_energy(file_name = 'pdb:1ajj.pqr', molecule_pqr_addres
     save_name = currDir + '/results/' + molecule_name
     io.write_vtk_manual(eval_gstate, log, filename=save_name)
     
-    SFE = get_free_energy(eval_gstate, psi_hat, atom_xyz_rad_chg)
+    
+    grad_psi_hat = sim_state.grad_solution
+    grad_psi_star = grad_psi_star_vec_fn(eval_gstate.R)
+    def get_epsilon_E_sq_field(r, g_hat_r, g_star_r):
+      phi_at_r = phi_fn(r)
+      return jnp.where(phi_at_r>0, mu_p_fn(r) * jnp.dot(g_hat_r, g_hat_r), mu_m_fn(r) * jnp.dot(g_hat_r + g_star_r, g_hat_r + g_star_r) )
+    epsilon_grad_psi_sq = vmap(get_epsilon_E_sq_field, (0, 0, 0))(eval_gstate.R, grad_psi_hat, grad_psi_star)
+    
+    def get_epsilon_E_coul_sq_field(r, g_star_r):
+      phi_at_r = phi_fn(r)
+      return jnp.where(phi_at_r>0, mu_p_fn(r) * jnp.dot(g_star_r, g_star_r), mu_m_fn(r) * jnp.dot(g_star_r, g_star_r) )
+    epsilon_grad_psi_star_sq = vmap(get_epsilon_E_coul_sq_field, (0, 0))(eval_gstate.R, grad_psi_star)
+    epsilon_grad_psi_hat_sq = vmap(get_epsilon_E_coul_sq_field, (0, 0))(eval_gstate.R, grad_psi_hat)
+    
+    SFE = get_free_energy(eval_gstate, eval_phi, psi_hat, atom_xyz_rad_chg, epsilon_grad_psi_sq, psi_solution, epsilon_grad_psi_star_sq, epsilon_grad_psi_hat_sq)
     train_grid_size = (xmax - xmin)/Nx_tr
     print(f"Molecule = {file_name} \t grid spacing (h_g) = {train_grid_size} (Angstrom) \t Solvation Free Energy = {SFE} (kcal/mol/e_C)")
     
@@ -241,7 +255,7 @@ def biomolecule_solvation_energy(file_name = 'pdb:1ajj.pqr', molecule_pqr_addres
 
 if __name__ == "__main__":
   
-    if False:
+    if True:
         """ For testing only run 1 molecule """
         biomolecule_solvation_energy()
         

src/nn_solution_model.py
@@ -19,9 +19,12 @@
 """
 
 import haiku as hk
-from jax import (numpy as jnp, nn as jnn)
+from jax import (numpy as jnp, random)
 from jax import config
 config.update("jax_debug_nans", False)
+
+import pdb
+
 
 from typing import Optional
 
@@ -41,10 +44,17 @@ class DoubleMLP(hk.Module):
         self.activation_fn = jnp.sin
         self.tr_normal_init = hk.initializers.TruncatedNormal(stddev=0.1, mean=0.0)
 
-        self.L = 3
-        self.args = 2**jnp.arange(self.L) * jnp.pi
-        self.encoding_m = jnp.zeros(6*self.L)
-        self.encoding_p = jnp.zeros(6*self.L)
+        d1 = 3                             # dimension of input space; e.g., 3D space
+        self.d2 = 16                       # dimension of lifted space
+        key = random.PRNGKey(0)
+        cov = jnp.eye(d1)
+        mean = jnp.zeros(d1)
+        
+        self.Bmat = random.multivariate_normal(key, mean, cov, shape=(self.d2,))
+        self.twoPi = 2.0 * jnp.pi
+
+        self.encoding_m = jnp.zeros(2*self.d2)
+        self.encoding_p = jnp.zeros(2*self.d2)
 
 
 
@@ -65,7 +75,7 @@ class DoubleMLP(hk.Module):
         output:
             one scalar value representing the solution u_p
         '''
-        # h = self.positional_encoding_p(h)
+        h = self.positional_encoding_p(h)
         for _ in range(self.num_hidden_layers):
             h = hk.Linear(output_size=self.hidden_dim, with_bias=True, w_init=self.tr_normal_init)(h)
             h = layer_norm(h)
@@ -81,7 +91,7 @@ class DoubleMLP(hk.Module):
         output:
             one scalar value representing the solution u_m
         '''
-        # h = self.positional_encoding_m(h)
+        h = self.positional_encoding_m(h)
         for _ in range(self.num_hidden_layers):
             h = hk.Linear(output_size=self.hidden_dim, with_bias=True, w_init=self.tr_normal_init)(h)
             h = layer_norm(h)
@@ -96,20 +106,11 @@ class DoubleMLP(hk.Module):
         input:
             h: vector of coordinates for one point (x,y,z)
         output:
-            L dimensional encoding
+            2*d2 dimensional encoding
         '''
-        x_sin = jnp.sin(self.args * h[0]); x_cos = jnp.cos(self.args * h[0])
-        y_sin = jnp.sin(self.args * h[1]); y_cos = jnp.cos(self.args * h[1])
-        z_sin = jnp.sin(self.args * h[2]); z_cos = jnp.cos(self.args * h[2])
-
-        self.encoding_p = self.encoding_p.at[:self.L].set(x_sin)
-        self.encoding_p = self.encoding_p.at[self.L:2*self.L].set(x_cos)
-
-        self.encoding_p = self.encoding_p.at[2*self.L:3*self.L].set(y_sin)
-        self.encoding_p = self.encoding_p.at[3*self.L:4*self.L].set(y_cos)
-
-        self.encoding_p = self.encoding_p.at[4*self.L:5*self.L].set(z_sin)
-        self.encoding_p = self.encoding_p.at[5*self.L:        ].set(z_cos)
+        arg = self.twoPi * (self.Bmat @ h)
+        self.encoding_p = self.encoding_p.at[:self.d2].set(jnp.sin(arg))
+        self.encoding_p = self.encoding_p.at[self.d2:].set(jnp.cos(arg))
         return self.encoding_p
 
     def positional_encoding_m(self, h):
@@ -118,20 +119,11 @@ class DoubleMLP(hk.Module):
         input:
             h: vector of coordinates for one point (x,y,z)
         output:
-            L dimensional encoding
+            2*d2 dimensional encoding
         '''
-        x_sin = jnp.sin(self.args * h[0]); x_cos = jnp.cos(self.args * h[0])
-        y_sin = jnp.sin(self.args * h[1]); y_cos = jnp.cos(self.args * h[1])
-        z_sin = jnp.sin(self.args * h[2]); z_cos = jnp.cos(self.args * h[2])
-
-        self.encoding_m = self.encoding_m.at[:self.L].set(x_sin)
-        self.encoding_m = self.encoding_m.at[self.L:2*self.L].set(x_cos)
-
-        self.encoding_m = self.encoding_m.at[2*self.L:3*self.L].set(y_sin)
-        self.encoding_m = self.encoding_m.at[3*self.L:4*self.L].set(y_cos)
-
-        self.encoding_m = self.encoding_m.at[4*self.L:5*self.L].set(z_sin)
-        self.encoding_m = self.encoding_m.at[5*self.L:        ].set(z_cos)
+        arg = self.twoPi * (self.Bmat @ h)
+        self.encoding_m = self.encoding_m.at[:self.d2].set(jnp.sin(arg))
+        self.encoding_m = self.encoding_m.at[self.d2:].set(jnp.cos(arg))
         return self.encoding_m
 
 